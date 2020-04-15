08b4fb3a462763d1fa020eafac20555a7e753129
pddla <chippermonky@gmail.com>
2020-04-15 10:57:25 -0700

    added reflex host testing and potato repeatEvent that I don't know how to meet the type constraints for

--- src/Reflex/Potato/Helpers.hs
+++ src/Reflex/Potato/Helpers.hs
@@ -1,9 +1,13 @@
--- various potato helpers
+{-# LANGUAGE RecursiveDo #-}
+
+
 module Reflex.Potato.Helpers (
   -- reflex helpers
   leftmostwarn
   , foldDynMergeWith
   , foldDynMerge
+
+  , repeatEvent
 ) where
 
 import           Relude
@@ -32,3 +36,26 @@ foldDynMerge :: (Reflex t, MonadHold t m, MonadFix m)
   -> [Event t a] -- ^ list of events
   -> m (Dynamic t b) -- ^ final output
 foldDynMerge f acc evs = foldDynMergeWith acc (f <<$>> evs)
+
+
+
+-- TODO figure out how to actually use this...
+-- | triggers output event once for each input event
+-- each output event runs in a different consecutive frame
+-- if these events trigger the input event, they get appended to the end of events to be triggered
+repeatEvent :: (Response m ~ Identity, Request m ~ Identity, Reflex t, Requester t m) => Event t [a] -> m (Event t a)
+repeatEvent evin = mdo
+  let
+    -- if input event fires in subsequent ticks, append to end
+    -- obviously, be mindful of infinite loops
+    evin' = mergeWith (\rev' ev' -> rev' <> ev') [rev, evin]
+    selectNext []    = Nothing
+    selectNext (x:_) = Just x
+    selectRest []     = Nothing
+    selectRest (_:[]) = Nothing
+    selectRest (_:xs) = Just xs
+    next = fmapMaybe selectNext evin'
+    rest = fmapMaybe selectRest evin'
+  -- does this mess with keys in DSum when multiple of these trigger?
+  rev <- requestingIdentity (Identity <$> rest)
+  requestingIdentity (Identity <$> next)
---
+++ test/Reflex/Potato/PotatoSpec.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE RecursiveDo #-}
+
+module Reflex.Potato.PotatoSpec (
+  spec
+) where
+
+import           Relude
+
+import           Test.Hspec
+import           Test.Hspec.Contrib.HUnit (fromHUnitTest)
+import           Test.HUnit
+
+import           Reflex
+import           Reflex.Potato.Helpers
+
+import           Reflex.Host.Basic
+
+repeatEvent_network :: forall t m. BasicGuestConstraints t m => Event t [Int] -> BasicGuest t m (Event t Int)
+repeatEvent_network = repeatEvent
+
+test_repeatEvent :: Test
+test_repeatEvent = TestLabel "basic" $ TestCase $ do
+  return ()
+  {-
+  let
+    bs = [[1..(10::Int)]]
+    run :: IO [[Maybe Int]]
+    run = basicHostWithStaticEvents bs repeatEvent_network
+  v <- liftIO run
+  print v
+  return ()
+  --L.last v @?= Just 103
+  -}
+
+spec :: Spec
+spec = do
+  describe "Potato" $ do
+    fromHUnitTest test_repeatEvent
