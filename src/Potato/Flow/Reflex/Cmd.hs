18b70f93b3f43b1fa3a44ee7935f4add604b127e
pdlla <chippermonky@gmail.com>
2020-04-18 16:18:44 -0700

    manually added GEq and GCompare instance for PFCmd ugg. Will switch to TH implementation someday

--- src/Potato/Flow/Reflex/Cmd.hs
+++ src/Potato/Flow/Reflex/Cmd.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE RecordWildCards    #-}
 {-# LANGUAGE RecursiveDo        #-}
+{-# LANGUAGE TemplateHaskell    #-}
 
 module Potato.Flow.Reflex.Cmd (
   PFCmdTag(..)
@@ -10,6 +11,8 @@ module Potato.Flow.Reflex.Cmd (
   , PFCmdStack
   , selectDo
   , selectUndo
+
+  --, TESTTag(..)
 ) where
 
 import           Relude
@@ -28,6 +31,21 @@ import           Potato.Flow.SElts
 import qualified Data.Dependent.Map              as DM
 import qualified Data.Dependent.Sum              as DS
 import qualified Data.GADT.Compare
+import           Data.GADT.Compare.TH
+
+
+{- DELETE
+
+import           Data.Maybe                      (fromJust)
+import           Language.Haskell.TH
+
+data TESTTag a where
+  TT1 :: TESTTag Int
+  TT2 :: TESTTag String
+  TT3 :: TESTTag Int
+deriveGEq      (fromJust <$> lookupTypeName "TESTTag")
+deriveGCompare ''TESTTag
+-}
 
 data PFCmdTag t a where
   PFCNewElts :: PFCmdTag t (NonEmpty (REltLabel t)) -- TODO needs LayerPos
@@ -38,8 +56,32 @@ data PFCmdTag t a where
   PFCManipulate :: PFCmdTag t (ControllerWithId)
   -- TODO maybe there is better type that can make use of Delta type class for scaling many
   PFCManipulateMany :: PFCmdTag t ([REltId], (LBox, LBox)) -- ^ (before, after)
-  deriving anyclass Data.GADT.Compare.GEq
-  deriving anyclass DM.GCompare
+  --deriving anyclass Data.GADT.Compare.GEq
+  --deriving anyclass DM.GCompare
+instance Generic  (PFCmdTag t a)
+
+instance Data.GADT.Compare.GEq (PFCmdTag t) where
+  geq PFCNewElts PFCNewElts               = do return Data.GADT.Compare.Refl
+  geq PFCDeleteElt PFCDeleteElt           = do return Data.GADT.Compare.Refl
+  geq PFCManipulate PFCManipulate         = do return Data.GADT.Compare.Refl
+  geq PFCManipulateMany PFCManipulateMany = do return Data.GADT.Compare.Refl
+
+instance Data.GADT.Compare.GCompare (PFCmdTag t) where
+  gcompare PFCNewElts PFCNewElts = runGComparing $ (do return Data.GADT.Compare.GEQ)
+  gcompare PFCNewElts _ = Data.GADT.Compare.GLT
+  gcompare _ PFCNewElts = Data.GADT.Compare.GGT
+  gcompare PFCDeleteElt PFCDeleteElt = runGComparing $ (do return Data.GADT.Compare.GEQ)
+  gcompare PFCDeleteElt _ = Data.GADT.Compare.GLT
+  gcompare _ PFCDeleteElt = Data.GADT.Compare.GGT
+  gcompare PFCManipulate PFCManipulate = runGComparing $ (do return Data.GADT.Compare.GEQ)
+  gcompare PFCManipulate _ = Data.GADT.Compare.GLT
+  gcompare _ PFCManipulate = Data.GADT.Compare.GGT
+  gcompare PFCManipulateMany PFCManipulateMany = runGComparing $ (do return Data.GADT.Compare.GEQ)
+  gcompare PFCManipulateMany _ = Data.GADT.Compare.GLT
+  gcompare _ PFCManipulateMany = Data.GADT.Compare.GGT
+
+
+
 
 type PFCmd t = DS.DSum (PFCmdTag t) Identity
 type PFCmdMap t = DM.DMap (PFCmdTag t) Identity
