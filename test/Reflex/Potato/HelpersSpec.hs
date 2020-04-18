9d5da6ee9bc0b395322e436e76ec763959b27e39
pdlla <chippermonky@gmail.com>
2020-04-17 19:53:04 -0700

    added test for repeatEventAndCollectOutput

--- test/Reflex/Potato/HelpersSpec.hs
+++ test/Reflex/Potato/HelpersSpec.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RecursiveDo #-}
 
-module Reflex.Potato.PotatoSpec (
+module Reflex.Potato.HelpersSpec (
   spec
 ) where
 
@@ -17,11 +17,26 @@ import           Reflex.Potato.Helpers
 
 import           Reflex.Host.Basic
 
+
+repeatEventAndCollectOutput_network :: forall t m. BasicGuestConstraints t m => Event t [Int] -> BasicGuest t m (Event t [Int])
+repeatEventAndCollectOutput_network ev = mdo
+  (repeated, collected) <- repeatEventAndCollectOutput ev repeated
+  return collected
+
+test_repeatEventAndCollectOutput :: Test
+test_repeatEventAndCollectOutput = TestLabel "repeatEventAndCollectOutput" $ TestCase $ do
+  let
+    bs = [[0],[],[1..5],[],[],[1,2],[1..10]] :: [[Int]]
+    run :: IO [[Maybe Int]]
+    run = basicHostWithStaticEvents bs repeatEvent_network
+  v <- liftIO run
+  fmap (filter isJust) v @?= Just <<$>> bs
+
 repeatEvent_network :: forall t m. BasicGuestConstraints t m => Event t [Int] -> BasicGuest t m (Event t Int)
 repeatEvent_network = repeatEvent
 
 test_repeatEvent :: Test
-test_repeatEvent = TestLabel "basic" $ TestCase $ do
+test_repeatEvent = TestLabel "repeatEvent" $ TestCase $ do
   let
     bs = [[1..10],[0],[],[1..5],[],[],[1,2]] :: [[Int]]
     run :: IO [[Maybe Int]]
@@ -35,3 +50,4 @@ spec :: Spec
 spec = do
   describe "Potato" $ do
     fromHUnitTest test_repeatEvent
+    fromHUnitTest test_repeatEventAndCollectOutput
