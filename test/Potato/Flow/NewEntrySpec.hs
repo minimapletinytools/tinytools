61e331d248de55581764d884a847cfe015ba7125
pdlla <chippermonky@gmail.com>
2020-04-25 09:57:38 -0700

    split off testing stuff into its own module

--- test/Potato/Flow/NewEntrySpec.hs
+++ test/Potato/Flow/NewEntrySpec.hs
@@ -15,110 +15,14 @@ import           Test.HUnit
 import           Reflex
 import           Reflex.Test.Host
 
-import           Data.Dependent.Sum       ((==>))
-import qualified Data.IntMap.Strict       as IM
-import qualified Data.List                as L (last, (!!))
-import qualified Data.List.Index          as L
-import           Data.Maybe               (fromJust)
+import qualified Data.List                as L (last)
 import qualified Data.Text                as T
 import           Data.These
 --import           Text.Pretty.Simple       (pPrint)
 
-import qualified Control.Monad.Random     as R
-
 import           Potato.Flow
+import           Potato.Flow.Testing
 
-simpleSBox :: SBox
-simpleSBox = SBox (LBox (LPoint (V2 5 5)) (LSize (V2 5 5))) defaultSLineStyle
-
-data FCmd =
-  FCNone
-  -- TODO add position param
-  | FCAddElt SElt
-  -- TODO change to take a Selection
-  | FCDeleteElt Int
-  | FCUndo
-  | FCRedo
-  | FCSave
-
-  | FCCustom_Add_SBox_1
-  | FCCustom_CBox_1 LayerPos
-  deriving (Eq, Show)
-
-isElement :: SEltLabel -> Bool
-isElement (SEltLabel _ selt) = case selt of
-  SEltNone        -> False
-  SEltFolderStart -> False
-  SEltFolderEnd   -> False
-  _               -> True
-
-randomActionFCmd :: Bool -> SEltTree -> IO FCmd
-randomActionFCmd doundo stree = do
-  let
-    --nElts = length stree
-    eltsOnly = filter (isElement . snd) $  L.indexed stree
-    nCmds = if doundo then 5 else 3
-  rcmd :: Int <- R.getRandomR (0, nCmds-1)
-  if null eltsOnly || rcmd == 0
-    then do
-      --pos <- R.getRandomR (0, nElts)
-      return $ FCAddElt $ SEltBox simpleSBox
-    else do
-      rindex <- R.getRandomR (0, length eltsOnly - 1)
-      let (pos, (SEltLabel _ selt)) = eltsOnly L.!! rindex
-      case rcmd of
-        1 -> return $ FCDeleteElt pos
-        2 -> case selt of
-          SEltBox _ -> return $ FCCustom_CBox_1 pos
-          _         -> undefined
-          --_         -> return FCNone
-        3 -> return FCUndo
-        4 -> return FCRedo
-        _ -> undefined
-
-
-setup_network:: forall t m. (t ~ SpiderTimeline Global, m ~ SpiderHost Global)
-  => Event t FCmd -> PerformEventT t m (PFOutput t)
-setup_network ev = mdo
-  let
-    addEv = flip fmapMaybe ev $ \case
-      FCAddElt x -> Just (0, SEltLabel "blank" x)
-      FCCustom_Add_SBox_1 -> Just (0, SEltLabel "customsbox" (SEltBox simpleSBox))
-      _           -> Nothing
-
-    removeEv = flip fmapMaybe ev $ \case
-      FCDeleteElt x -> Just x
-      _              -> Nothing
-    manipEv = flip push ev $ \case
-      FCCustom_CBox_1 lp -> do
-        (_,rid,SEltLabel _ selt) <- fromJust <$> sEltLayerTree_sampleSuperSEltByPos layerTree lp
-        let
-          cbox = CBox {
-              _cBox_box    = DeltaLBox (LPoint (V2 1 1)) (LSize (V2 5 5))
-            }
-        return . Just $ IM.singleton rid (CTagBox ==> cbox)
-      _              -> return Nothing
-    redoEv = flip fmapMaybe ev $ \case
-      FCRedo -> Just ()
-      _      -> Nothing
-    undoEv = flip fmapMaybe ev $ \case
-      FCUndo -> Just ()
-      _      -> Nothing
-    saveEv = flip fmapMaybe ev $ \case
-      FCSave -> Just ()
-      _      -> Nothing
-
-    pfc = PFConfig { _pfc_addElt     = addEv
-                   , _pfc_removeElt  = removeEv
-                   , _pfc_manipulate = manipEv
-                   , _pfc_undo       = undoEv
-                   , _pfc_redo       = redoEv
-                   , _pfc_save = saveEv
-                   }
-  pfo <- holdPF pfc
-  let
-    layerTree = _pfo_layers $ pfo
-  return pfo
 
 
 save_network
@@ -163,19 +67,6 @@ pair_test name network (bs1, bs2) = TestLabel ("pairs: " ++ T.unpack name) $ Tes
   v1 <- liftIO run1
   v2 <- liftIO run2
   L.last (join v1) @?= L.last (join v2)
-
-
--- | make sure to tick with 'FCNone' to ensure output behavior is most recent
-step_state_network :: forall t m.
-  (t ~ SpiderTimeline Global, m ~ SpiderHost Global)
-  => (AppIn t () FCmd -> PerformEventT t m (AppOut t SEltTree ()))
-step_state_network AppIn {..} = do
-  pfo <- setup_network _appIn_event
-  return
-    AppOut {
-      _appOut_behavior = _pfo_state pfo
-      , _appOut_event  = never
-    }
 
 nstep_test :: forall t m.
   (t ~ SpiderTimeline Global, m ~ SpiderHost Global)
---
+++ test/Potato/Flow/Testing.hs
@@ -0,0 +1,130 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecursiveDo     #-}
+
+module Potato.Flow.Testing (
+  simpleSBox
+  , FCmd(..)
+  , randomActionFCmd
+  , setup_network
+  , step_state_network
+) where
+
+import           Relude               hiding (empty, fromList)
+
+import           Reflex
+import           Reflex.Test.Host
+
+import           Data.Dependent.Sum   ((==>))
+import qualified Data.IntMap.Strict   as IM
+import qualified Data.List            as L ((!!))
+import qualified Data.List.Index      as L
+import           Data.Maybe           (fromJust)
+
+import qualified Control.Monad.Random as R
+
+import           Potato.Flow
+
+simpleSBox :: SBox
+simpleSBox = SBox (LBox (LPoint (V2 5 5)) (LSize (V2 5 5))) defaultSLineStyle
+
+data FCmd =
+  FCNone
+  -- TODO add position param
+  | FCAddElt SElt
+  -- TODO change to take a Selection
+  | FCDeleteElt Int
+  | FCUndo
+  | FCRedo
+  | FCSave
+
+  | FCCustom_Add_SBox_1
+  | FCCustom_CBox_1 LayerPos
+  deriving (Eq, Show)
+
+isElement :: SEltLabel -> Bool
+isElement (SEltLabel _ selt) = case selt of
+  SEltNone        -> False
+  SEltFolderStart -> False
+  SEltFolderEnd   -> False
+  _               -> True
+
+randomActionFCmd :: Bool -> SEltTree -> IO FCmd
+randomActionFCmd doundo stree = do
+  let
+    --nElts = length stree
+    eltsOnly = filter (isElement . snd) $  L.indexed stree
+    nCmds = if doundo then 5 else 3
+  rcmd :: Int <- R.getRandomR (0, nCmds-1)
+  if null eltsOnly || rcmd == 0
+    then do
+      --pos <- R.getRandomR (0, nElts)
+      return $ FCAddElt $ SEltBox simpleSBox
+    else do
+      rindex <- R.getRandomR (0, length eltsOnly - 1)
+      let (pos, (SEltLabel _ selt)) = eltsOnly L.!! rindex
+      case rcmd of
+        1 -> return $ FCDeleteElt pos
+        2 -> case selt of
+          SEltBox _ -> return $ FCCustom_CBox_1 pos
+          _         -> undefined
+          --_         -> return FCNone
+        3 -> return FCUndo
+        4 -> return FCRedo
+        _ -> undefined
+
+
+setup_network:: forall t m. (t ~ SpiderTimeline Global, m ~ SpiderHost Global)
+  => Event t FCmd -> PerformEventT t m (PFOutput t)
+setup_network ev = mdo
+  let
+    addEv = flip fmapMaybe ev $ \case
+      FCAddElt x -> Just (0, SEltLabel "blank" x)
+      FCCustom_Add_SBox_1 -> Just (0, SEltLabel "customsbox" (SEltBox simpleSBox))
+      _           -> Nothing
+
+    removeEv = flip fmapMaybe ev $ \case
+      FCDeleteElt x -> Just x
+      _              -> Nothing
+    manipEv = flip push ev $ \case
+      FCCustom_CBox_1 lp -> do
+        (_,rid,SEltLabel _ selt) <- fromJust <$> sEltLayerTree_sampleSuperSEltByPos layerTree lp
+        let
+          cbox = CBox {
+              _cBox_box    = DeltaLBox (LPoint (V2 1 1)) (LSize (V2 5 5))
+            }
+        return . Just $ IM.singleton rid (CTagBox ==> cbox)
+      _              -> return Nothing
+    redoEv = flip fmapMaybe ev $ \case
+      FCRedo -> Just ()
+      _      -> Nothing
+    undoEv = flip fmapMaybe ev $ \case
+      FCUndo -> Just ()
+      _      -> Nothing
+    saveEv = flip fmapMaybe ev $ \case
+      FCSave -> Just ()
+      _      -> Nothing
+
+    pfc = PFConfig { _pfc_addElt     = addEv
+                   , _pfc_removeElt  = removeEv
+                   , _pfc_manipulate = manipEv
+                   , _pfc_undo       = undoEv
+                   , _pfc_redo       = redoEv
+                   , _pfc_save = saveEv
+                   }
+  pfo <- holdPF pfc
+  let
+    layerTree = _pfo_layers $ pfo
+  return pfo
+
+
+-- | make sure to tick with 'FCNone' to ensure output behavior is most recent
+step_state_network :: forall t m.
+  (t ~ SpiderTimeline Global, m ~ SpiderHost Global)
+  => (AppIn t () FCmd -> PerformEventT t m (AppOut t SEltTree ()))
+step_state_network AppIn {..} = do
+  pfo <- setup_network _appIn_event
+  return
+    AppOut {
+      _appOut_behavior = _pfo_state pfo
+      , _appOut_event  = never
+    }
