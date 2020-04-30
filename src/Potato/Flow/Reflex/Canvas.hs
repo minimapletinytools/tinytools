b536df2589ebf16dbacaa2cec06388267cb81536
pdlla <chippermonky@gmail.com>
2020-04-30 13:09:43 -0700

    added Canvas

---
+++ src/Potato/Flow/Reflex/Canvas.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecursiveDo     #-}
+
+
+module Potato.Flow.Reflex.Canvas (
+  Canvas(..)
+  , CanvasConfig(..)
+  , holdCanvas
+) where
+
+import           Relude
+
+import           Reflex
+
+import           Potato.Flow.Math
+import           Potato.Flow.Reflex.Types
+
+import           Control.Monad.Fix
+
+data Canvas t = Canvas {
+  _canvas_box :: Dynamic t LBox
+}
+
+data CanvasConfig t = CanvasConfig {
+  _canvasConfig_resize :: Event t (LBox -> LBox)
+}
+
+holdCanvas :: forall t m. (Adjustable t m, MonadHold t m, MonadFix m)
+  => CanvasConfig t
+  -> m (Canvas t)
+holdCanvas CanvasConfig {..} = mdo
+  let
+    canvasBox0 = LBox (LPoint (V2 0 0)) (LSize (V2 100 50))
+  canvasBox <- foldDyn ($) canvasBox0 _canvasConfig_resize
+  return
+    Canvas {
+      _canvas_box = canvasBox
+    }
