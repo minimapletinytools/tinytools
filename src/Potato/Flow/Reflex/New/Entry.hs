b3689c131e7ca234817f38c2219db3e9307453ac
pdlla <chippermonky@gmail.com>
2020-07-06 19:47:53 -0700

    combine all Events into PFEventCmd

--- src/Potato/Flow/Reflex/New/Entry.hs
+++ src/Potato/Flow/Reflex/New/Entry.hs
@@ -59,8 +59,7 @@ potato_simplifyPotatoTotal PotatoTotal {..} = r where
   foldfn index rid acc = (rid, index, (_potatoTotal_sEltLabelMap IM.! rid)) : acc
   r = Seq.foldrWithIndex foldfn [] _potatoTotal_layers
 
-
-
+-- _pfc_([^\s]+)\s+:: Event t\s.*
 data PFConfig t = PFConfig {
   --_pfc_setWorkspace :: SetWSEvent t
   -- | don't use this to add folders, as they need to be added with matching pair to ensure scoping property
@@ -68,9 +67,9 @@ data PFConfig t = PFConfig {
   , _pfc_addFolder    :: Event t (LayerPos, Text)
   , _pfc_removeElt    :: Event t [LayerPos]
   , _pfc_moveElt      :: Event t ([LayerPos], LayerPos) -- new layer position is before removal
-  , _pfc_copy         :: Event t [LayerPos]
+  --, _pfc_copy         :: Event t [LayerPos]
   , _pfc_paste        :: Event t ([SElt], LayerPos)
-  , _pfc_duplicate    :: Event t [LayerPos]
+  --, _pfc_duplicate    :: Event t [LayerPos]
   , _pfc_manipulate   :: Event t ControllersWithId
   , _pfc_resizeCanvas :: Event t DeltaLBox
   , _pfc_undo         :: Event t ()
@@ -96,9 +95,42 @@ data PFOutput t = PFOutput {
   , _pfo_potato_potatoTotal :: Dynamic t PotatoTotal
 }
 
+
+data PFEventCmd =
+  PFEAddElt (LayerPos, SEltLabel)
+  | PFEAddFolder (LayerPos, Text)
+  | PFERemoveElt [LayerPos]
+  | PFEMoveElt ([LayerPos], LayerPos)
+  -- | PFECopy [LayerPos]
+  | PFEPaste ([SElt], LayerPos)
+  -- | PFEDuplicate [LayerPos]
+  | PFEManipulate ControllersWithId
+  | PFEResizeCanvas DeltaLBox
+  | PFEUndo
+  | PFERedo
+  | PFELoad SPotatoFlow
+  | PFESave
+
+
 holdPF ::
   forall t m. (Reflex t, Adjustable t m, MonadHold t m, MonadFix m)
   => PFConfig t
   -> m (PFOutput t)
 holdPF PFConfig {..} = mdo
+  let
+    pfccmd = leftmostWarn "PFConfig"
+      [ PFEAddElt <$> _pfc_addElt
+      , PFEAddFolder <$> _pfc_addFolder
+      , PFERemoveElt <$> _pfc_removeElt
+      , PFEMoveElt <$> _pfc_moveElt
+      -- , PFECopy <$> _pfc_copy
+      , PFEPaste <$> _pfc_paste
+      -- , PFEDuplicate <$> _pfc_duplicate
+      , PFEManipulate <$> _pfc_manipulate
+      , PFEResizeCanvas <$> _pfc_resizeCanvas
+      , PFEUndo <$ _pfc_undo
+      , PFERedo <$ _pfc_redo
+      , PFELoad <$> _pfc_load
+      , PFESave <$ _pfc_save]
+
   undefined
