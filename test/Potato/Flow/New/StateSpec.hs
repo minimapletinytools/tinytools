5a3bf507fcab585c3de73b66a2a6037c30314dec
pdlla <chippermonky@gmail.com>
2020-08-02 08:29:34 -0700

    added tests for pFState_isValid

--- test/Potato/Flow/New/StateSpec.hs
+++ test/Potato/Flow/New/StateSpec.hs
@@ -8,11 +8,45 @@ import           Relude
 
 import           Test.Hspec
 
+import qualified Data.IntMap              as IM
 import qualified Data.Sequence            as Seq
 import           Potato.Flow
 
 import           Potato.Flow.New.State
 import           Potato.Flow.Reflex.Types
+
+
+folderStart :: SEltLabel
+folderStart = SEltLabel "folder" SEltFolderStart
+
+folderEnd :: SEltLabel
+folderEnd = SEltLabel "folder (end)" SEltFolderEnd
+
+someSEltLabel :: SEltLabel
+someSEltLabel = SEltLabel "some elt" SEltNone
+
+someValidState1 :: PFState
+someValidState1 = PFState {
+      _pFState_layers = Seq.fromList [0..5]
+      , _pFState_directory = IM.fromList [(0, folderStart), (1, folderEnd), (2, someSEltLabel), (3, someSEltLabel), (4, someSEltLabel), (5, someSEltLabel)]
+      , _pFState_canvas = SCanvas defaultCanvasLBox
+  }
+
+someInvalidState1 :: PFState
+someInvalidState1 = PFState {
+      _pFState_layers = Seq.fromList [0..5]
+      -- missing REltId 5
+      , _pFState_directory = IM.fromList [(0, folderStart), (1, folderEnd), (2, someSEltLabel), (3, someSEltLabel), (4, someSEltLabel)]
+      , _pFState_canvas = SCanvas defaultCanvasLBox
+  }
+
+someInvalidState2 :: PFState
+someInvalidState2 = PFState {
+      -- folder mismatch
+      _pFState_layers = Seq.fromList [0..5]
+      , _pFState_directory = IM.fromList [(0, folderStart), (1, folderStart), (2, someSEltLabel), (3, someSEltLabel), (4, someSEltLabel)]
+      , _pFState_canvas = SCanvas defaultCanvasLBox
+  }
 
 spec :: Spec
 spec = do
@@ -21,3 +55,7 @@ spec = do
       let
         orig = SPotatoFlow (SCanvas (LBox 10 123)) [SEltLabel "some selt" SEltNone]
       pFState_to_sPotatoFlow (sPotatoFlow_to_pFState orig) `shouldBe` orig
+    it "pFState_isValid" $ do
+      pFState_isValid someValidState1 `shouldBe` True
+      pFState_isValid someInvalidState1 `shouldBe` False
+      pFState_isValid someInvalidState2 `shouldBe` False
