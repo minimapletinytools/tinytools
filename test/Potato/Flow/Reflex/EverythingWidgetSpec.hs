f447e20faca85e9ded0a7797acb2d7a4416f4888
pdlla <chippermonky@gmail.com>
2020-11-06 17:40:00 -0800

    split test stuff into common file

--- test/Potato/Flow/Reflex/EverythingWidgetSpec.hs
+++ test/Potato/Flow/Reflex/EverythingWidgetSpec.hs
@@ -20,6 +20,9 @@ import           Potato.Flow.Reflex.Everything
 import           Potato.Flow.Reflex.EverythingWidget
 import           Potato.Flow.TestStates
 
+-- test imports
+import           Potato.Flow.Reflex.Common
+
 import           Control.Monad.Fix
 import qualified Data.IntMap                         as IM
 import qualified Data.Sequence                       as Seq
@@ -77,136 +80,7 @@ select_test = TestLabel "select" $ TestCase $ do
   v <- liftIO run
   (join v) @?= expected
 
--- generic testing
-data EverythingWidgetCmd =
-  EWCMouse LMouseData
-  | EWCKeyboard KeyboardData
-  | EWCTool Tool
-  | EWCSelect Selection Bool -- true if add to selection
-  | EWCNothing
 
-
-
-everything_network_app
-  :: forall t m. (t ~ SpiderTimeline Global, m ~ SpiderHost Global)
-  => AppIn t () EverythingWidgetCmd -> TestGuestT t m (AppOut t EverythingCombined_DEBUG EverythingCombined_DEBUG)
-everything_network_app (AppIn _ ev) = do
-  let ewc = EverythingWidgetConfig  {
-      _everythingWidgetConfig_initialState = emptyPFState
-
-      , _everythingWidgetConfig_mouse = fforMaybe ev $ \case
-        EWCMouse x -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_keyboard = fforMaybe ev $ \case
-        EWCKeyboard x -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_selectTool = fforMaybe ev $ \case
-        EWCTool x -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_selectNew = fforMaybe ev $ \case
-        EWCSelect x False -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_selectAdd = fforMaybe ev $ \case
-        EWCSelect x True -> Just x
-        _ -> Nothing
-    }
-  everythingWidget <- holdEverythingWidget ewc
-  let rDyn = _everythingWidget_everythingCombined_DEBUG everythingWidget
-  return $ AppOut (current rDyn) (updated rDyn)
-
-
--- simplified version of above with no behavior, you can delete this
-everything_network
-  :: forall t m. (t ~ SpiderTimeline Global, m ~ SpiderHost Global)
-  => Event t EverythingWidgetCmd -> TestGuestT t m (Event t EverythingCombined_DEBUG)
-everything_network ev = do
-  let ewc = EverythingWidgetConfig  {
-      _everythingWidgetConfig_initialState = emptyPFState
-
-      , _everythingWidgetConfig_mouse = fforMaybe ev $ \case
-        EWCMouse x -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_keyboard = fforMaybe ev $ \case
-        EWCKeyboard x -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_selectTool = fforMaybe ev $ \case
-        EWCTool x -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_selectNew = fforMaybe ev $ \case
-        EWCSelect x False -> Just x
-        _ -> Nothing
-      , _everythingWidgetConfig_selectAdd = fforMaybe ev $ \case
-        EWCSelect x True -> Just x
-        _ -> Nothing
-    }
-  everythingWidget <- holdEverythingWidget ewc
-  return $ updated $ _everythingWidget_everythingCombined_DEBUG everythingWidget
-
-data EverythingPredicate where
-  EqPredicate :: (Show a, Eq a) => (EverythingCombined_DEBUG -> a) -> a -> EverythingPredicate
-  FunctionPredicate :: (EverythingCombined_DEBUG -> (Text, Bool)) -> EverythingPredicate
-  PFStateFunctionPredicate :: (PFState -> (Text, Bool)) -> EverythingPredicate
-  AlwaysPass :: EverythingPredicate
-  Combine :: [EverythingPredicate] -> EverythingPredicate
-
-testEverythingPredicate :: EverythingPredicate -> EverythingCombined_DEBUG -> Bool
-testEverythingPredicate (EqPredicate f a) e     = f e == a
-testEverythingPredicate (FunctionPredicate f) e = snd $ f e
-testEverythingPredicate (PFStateFunctionPredicate f) e = snd . f $ _everythingCombined_pFState e
-testEverythingPredicate AlwaysPass _            = True
-testEverythingPredicate (Combine xs) e          = all id . map (\p -> testEverythingPredicate p e) $ xs
-
-showEverythingPredicate :: EverythingPredicate -> EverythingCombined_DEBUG -> Text
-showEverythingPredicate (EqPredicate f a) e = "expected: " <> show a <> " got: " <> show (f e)
-showEverythingPredicate (FunctionPredicate f) e = fst $ f e
-showEverythingPredicate (PFStateFunctionPredicate f) e = fst . f $ _everythingCombined_pFState e
-showEverythingPredicate AlwaysPass _ = "always pass"
-showEverythingPredicate (Combine xs) e = "[" <> foldr (\p acc -> showEverythingPredicate p e <> ", " <> acc) "" xs <> "]"
-
-
--- umm, is there a better way to do this? Too bad you can't pass a pattern match as an argument or can you?
-data LastOperationType = LastOperationType_Manipulate | LastOperationType_Undo | LastOperationType_None
-checkLastOperationPredicate :: LastOperationType -> EverythingPredicate
-checkLastOperationPredicate operation = case operation of
-  LastOperationType_Manipulate -> FunctionPredicate (
-    (\case
-      FrontendOperation_Manipulate _ _ -> ("",True)
-      o -> ("Expected FrontendOperation_Manipulate got " <> show o, False))
-    . _everythingCombined_lastOperation)
-  LastOperationType_None -> FunctionPredicate (
-    (\case
-      FrontendOperation_None -> ("",True)
-      o -> ("Expected FrontendOperation_None got " <> show o, False))
-    . _everythingCombined_lastOperation)
-  LastOperationType_Undo -> FunctionPredicate (
-    (\case
-      FrontendOperation_Undo -> ("",True)
-      o -> ("Expected FrontendOperation_Undo got " <> show o, False))
-    . _everythingCombined_lastOperation)
-
-checkNumElts :: Int -> PFState -> (Text, Bool)
-checkNumElts n PFState {..} = (t,r) where
-  ds = IM.size _pFState_directory
-  ls = Seq.length _pFState_layers
-  r = ds == n && ls == n
-  t = "expected: " <> show n <> " dir: " <> show ds <> " layers: " <> show ls
-
-numSelectedEltsEqualPredicate :: Int -> EverythingPredicate
-numSelectedEltsEqualPredicate n = FunctionPredicate $
-  (\s ->
-    let nSelected = Seq.length s
-    in ("Selected: " <> show nSelected <> " expected: " <> show n, nSelected == n))
-  . _everythingCombined_selection
-
-firstSelectedSuperSEltLabelPredicate :: (SuperSEltLabel -> Bool) -> EverythingPredicate
-firstSelectedSuperSEltLabelPredicate f = FunctionPredicate $
-  (\s ->
-    let
-      mfirst = Seq.lookup 0 s
-    in case mfirst of
-      Nothing    -> ("Selection is empty", False)
-      Just first -> ("First selected: " <> show first, f first))
-  . _everythingCombined_selection
 
 everything_basic_test :: Test
 everything_basic_test = TestLabel "everything_basic" $ TestCase $ do
