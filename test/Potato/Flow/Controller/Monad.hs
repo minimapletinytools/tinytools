a9eea41d63044d66ef5f4c392dfb7aa640acba67
pdlla <chippermonky@gmail.com>
2020-12-31 11:38:30 -0800

    switch monad test to use existing types

---
+++ test/Potato/Flow/Controller/Monad.hs
@@ -0,0 +1,62 @@
+-- monadic testing types for GoatWidget
+
+{-# LANGUAGE RecordWildCards      #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Potato.Flow.Controller.Monad (
+  GoatWidgetTest
+  , runGoatTestApp
+) where
+
+import           Relude                            hiding (empty, fromList)
+
+import           Test.Hspec
+import           Test.Hspec.Contrib.HUnit          (fromHUnitTest)
+import           Test.HUnit
+
+import           Reflex
+import           Reflex.Host.Class
+import           Reflex.Test.Monad.Host
+
+import           Potato.Flow
+import           Potato.Flow.Controller.GoatWidget
+import           Potato.Flow.TestStates
+
+import           Control.Monad.Fix
+
+data GoatWidgetTest t (m :: Type -> Type)
+
+instance (TestGuestConstraints t m) => ReflexTestApp (GoatWidgetTest t m) t m where
+  data AppInputTriggerRefs (GoatWidgetTest t m) = GoatWidgetTest_InputTriggerRefs {
+      _goatWidgetTest_inputTriggerRefs_mouse         :: ReflexTriggerRef t m LMouseData
+      , _goatWidgetTest_inputTriggerRefs_keyboard      :: ReflexTriggerRef t m KeyboardData
+      , _goatWidgetTest_inputTriggerRefs_selectTool    :: ReflexTriggerRef t m Tool
+      , _goatWidgetTest_inputTriggerRefs_load          :: ReflexTriggerRef t m EverythingLoadState
+      , _goatWidgetTest_inputTriggerRefs_setDebugLabel :: ReflexTriggerRef t m Text
+    }
+  data AppInputEvents (GoatWidgetTest t m) = GoatWidgetTest_InputEvents (GoatWidgetConfig t)
+  data AppOutput (GoatWidgetTest t m) = GoatWidgetTest_Output (GoatWidget t)
+  getApp (GoatWidgetTest_InputEvents config) = do
+    goatWidget <- holdGoatWidget config
+    return $ GoatWidgetTest_Output goatWidget
+  makeInputs = do
+    (inmouse, intrefmouse) <- newEventWithTriggerRef
+    (inkb, intrefkb) <- newEventWithTriggerRef
+    (intool, intreftool) <- newEventWithTriggerRef
+    (inload, intrefload) <- newEventWithTriggerRef
+    (indebug, intrefdebug) <- newEventWithTriggerRef
+    return (
+        GoatWidgetTest_InputEvents (GoatWidgetConfig undefined inmouse inkb intool inload indebug)
+        , GoatWidgetTest_InputTriggerRefs intrefmouse intrefkb intreftool intrefload intrefdebug
+      )
+
+runGoatTestApp
+  :: (TestGuestConstraints t m)
+  => PFState
+  -> ReflexTestT t (AppInputTriggerRefs (GoatWidgetTest t m)) (AppOutput (GoatWidgetTest t m)) m ()
+  -> m ()
+runGoatTestApp initialState rtm = do
+  (GoatWidgetTest_InputEvents inev, intref) <- makeInputs
+  let
+    newConfig = GoatWidgetTest_InputEvents (inev { _goatWidgetConfig_initialState = initialState })
+  runReflexTestT (newConfig, intref) getApp rtm
