fa8a429cd6b124a872baa3ae7cde010269aaa734
pdlla <chippermonky@gmail.com>
2021-04-18 20:46:31 -0700

    OwlWorkspace compiles D:

---
+++ src/Potato/Flow/OwlWorkspace.hs
@@ -0,0 +1,175 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Potato.Flow.OwlWorkspace (
+  OwlPFWorkspace(..)
+  , emptyWorkspace
+  , emptyActionStack
+  , undoWorkspace
+  , redoWorkspace
+  , undoPermanentWorkspace
+  , doCmdWorkspace
+  , WSEvent(..)
+  , updateOwlPFWorkspace
+) where
+
+import           Relude
+
+import           Potato.Flow.Cmd
+import           Potato.Flow.Layers
+import           Potato.Flow.Math
+import           Potato.Flow.SElts
+import           Potato.Flow.Owl
+import           Potato.Flow.OwlState
+import           Potato.Flow.Types
+
+import           Control.Exception  (assert)
+import           Data.Dependent.Sum (DSum ((:=>)), (==>))
+import qualified Data.IntMap.Strict as IM
+import qualified Data.Sequence      as Seq
+
+
+
+data OwlPFCmd =
+  OwlPFCNewElts [(REltId, OwlSpot, OwlElt)]
+  | OwlPFCDeleteElts [(REltId, OwlSpot, OwlElt)]
+  | OwlPFCManipulate ControllersWithId
+  -- we need SuperOwlParliament for undo
+  | OwlPFCMove (OwlSpot, SuperOwlParliament)
+
+  | OwlPFCResizeCanvas DeltaLBox
+  deriving (Show, Generic)
+
+--instance NFData OwlPFCmd
+
+-- TODO move this into a diff file
+data ActionStack = ActionStack {
+  doStack     :: [OwlPFCmd] -- maybe just do something lke [PFCmd, Maybe OwlPFState] here for state based undo
+  , undoStack :: [OwlPFCmd]
+} deriving (Show, Generic)
+
+--instance NFData ActionStack
+
+emptyActionStack :: ActionStack
+emptyActionStack = ActionStack [] []
+
+data OwlPFWorkspace = OwlPFWorkspace {
+  _owlPFWorkspace_pFState       :: OwlPFState
+  , _owlPFWorkspace_lastChanges :: SuperOwlChanges
+  , _owlPFWorkspace_actionStack :: ActionStack
+} deriving (Show, Generic)
+
+--instance NFData OwlPFWorkspace
+
+loadOwlPFStateIntoWorkspace :: OwlPFState -> OwlPFWorkspace -> OwlPFWorkspace
+loadOwlPFStateIntoWorkspace pfs ws = r where
+  removeOld = fmap (const Nothing) (_owlTree_mapping . _owlPFState_owlTree . _owlPFWorkspace_pFState $ ws)
+  addNew = IM.mapWithKey (\rid (oem,oe) -> Just (SuperOwl rid oem oe)) (_owlTree_mapping . _owlPFState_owlTree $ pfs)
+  changes = IM.union addNew removeOld
+  r = OwlPFWorkspace pfs changes emptyActionStack
+
+emptyWorkspace :: OwlPFWorkspace
+emptyWorkspace = OwlPFWorkspace emptyPFState IM.empty emptyActionStack
+
+undoWorkspace :: OwlPFWorkspace -> OwlPFWorkspace
+undoWorkspace pfw =  r where
+  ActionStack {..} = _owlPFWorkspace_actionStack pfw
+  r = case doStack of
+    --c : cs -> trace "UNDO: " .traceShow c $ OwlPFWorkspace (undoCmdState c _owlPFWorkspace_pFState) (ActionStack cs (c:undoStack))
+    c : cs -> uncurry OwlPFWorkspace (undoCmdState c (_owlPFWorkspace_pFState pfw)) (ActionStack cs (c:undoStack))
+    _ -> pfw
+
+redoWorkspace :: OwlPFWorkspace -> OwlPFWorkspace
+redoWorkspace pfw = r where
+  ActionStack {..} = _owlPFWorkspace_actionStack pfw
+  r = case undoStack of
+    --c : cs -> trace "REDO: " . traceShow c $ OwlPFWorkspace (doCmdState c _owlPFWorkspace_pFState) (ActionStack (c:doStack) cs)
+    c : cs -> uncurry OwlPFWorkspace (doCmdState c (_owlPFWorkspace_pFState pfw)) (ActionStack (c:doStack) cs)
+    _ -> pfw
+
+undoPermanentWorkspace :: OwlPFWorkspace -> OwlPFWorkspace
+undoPermanentWorkspace pfw =  r where
+  ActionStack {..} = _owlPFWorkspace_actionStack pfw
+  r = case doStack of
+    --c : cs -> trace "UNDO: " .traceShow c $ OwlPFWorkspace (undoCmdState c _owlPFWorkspace_pFState) (ActionStack cs (c:undoStack))
+    c : cs -> uncurry OwlPFWorkspace (undoCmdState c (_owlPFWorkspace_pFState pfw)) (ActionStack cs undoStack)
+    _ -> pfw
+
+doCmdWorkspace :: OwlPFCmd -> OwlPFWorkspace -> OwlPFWorkspace
+--doCmdWorkspace cmd OwlPFWorkspace {..} = trace "DO: " . traceShow cmd $ r wherem
+-- TODO deepseq here to force evaluation of workspace and prevent leaks
+doCmdWorkspace cmd pfw = r where
+  newState = doCmdState cmd (_owlPFWorkspace_pFState pfw)
+  ActionStack {..} = (_owlPFWorkspace_actionStack pfw)
+  newStack = ActionStack (cmd:doStack) []
+  --newMaxId = pFState_maxID _owlPFWorkspace_pFState
+  r = uncurry OwlPFWorkspace newState newStack
+
+doCmdState :: OwlPFCmd -> OwlPFState -> (OwlPFState, SuperOwlChanges)
+doCmdState cmd s = assert (pFState_isValid newState) (newState, changes) where
+  (newState, changes) = case cmd of
+    OwlPFCNewElts x      ->  do_newElts x s
+    OwlPFCDeleteElts x   ->  do_deleteElts x s
+    OwlPFCManipulate x   ->  do_manipulate x s
+    OwlPFCMove x         -> do_move x s
+    OwlPFCResizeCanvas x -> (do_resizeCanvas x s, IM.empty)
+
+undoCmdState :: OwlPFCmd -> OwlPFState -> (OwlPFState, SuperOwlChanges)
+undoCmdState cmd s = assert (pFState_isValid newState) (newState, changes) where
+  (newState, changes) =  case cmd of
+    OwlPFCNewElts x      ->  undo_newElts x s
+    OwlPFCDeleteElts x   ->  undo_deleteElts x s
+    OwlPFCManipulate x   ->  undo_manipulate x s
+    OwlPFCMove x         -> undo_move x s
+    OwlPFCResizeCanvas x -> (undo_resizeCanvas x s, IM.empty)
+
+------ update functions via commands
+data WSEvent =
+  -- CHANGE TODO FIGURE IT OUT
+  WSEAddElt (Bool, OwlSpot, OwlElt)
+  | WSEAddRelative (OwlSpot, Seq OwlElt)
+  | WSEAddFolder (OwlSpot, Text)
+  | WSERemoveElt OwlParliament -- removed kiddos get adopted by grandparents or w/e?
+  | WSEMoveElt (OwlSpot, OwlParliament) -- also moves kiddos?
+  -- | WSEDuplicate OwlParliament -- kiddos get duplicated??
+  | WSEManipulate (Bool, ControllersWithId)
+  | WSEResizeCanvas DeltaLBox
+  | WSEUndo
+  | WSERedo
+  | WSELoad SPotatoFlow
+  deriving (Show)
+
+debugPrintBeforeAfterState :: (IsString a) => OwlPFState -> OwlPFState -> a
+debugPrintBeforeAfterState stateBefore stateAfter = fromString $ "BEFORE: " <> debugPrintPFState stateBefore <> "\nAFTER: " <> debugPrintPFState stateAfter
+
+doCmdOwlPFWorkspaceUndoPermanentFirst :: (OwlPFState -> OwlPFCmd) -> OwlPFWorkspace -> OwlPFWorkspace
+doCmdOwlPFWorkspaceUndoPermanentFirst cmdFn ws = r where
+  -- undoPermanent is actually not necessary as the next action clears the redo stack anyways
+  undoedws = undoPermanentWorkspace ws
+  undoedpfs = _owlPFWorkspace_pFState undoedws
+  cmd = cmdFn undoedpfs
+  r = doCmdWorkspace cmd undoedws
+
+updateOwlPFWorkspace :: WSEvent -> OwlPFWorkspace -> OwlPFWorkspace
+updateOwlPFWorkspace evt ws = let
+  lastState = _owlPFWorkspace_pFState ws
+  r = case evt of
+    WSEAddElt (undo, spot, oelt) -> if undo
+      then undefined --doCmdOwlPFWorkspaceUndoPermanentFirst (\pfs -> pfc_addElt_to_newElts pfs x) ws
+      else undefined --doCmdWorkspace (pfc_addElt_to_newElts lastState x) ws
+    WSEAddRelative x -> undefined --doCmdWorkspace (pfc_addRelative_to_newElts lastState x) ws
+    WSEAddFolder x -> undefined --doCmdWorkspace (pfc_addFolder_to_newElts lastState x) ws
+    WSERemoveElt x -> undefined --doCmdWorkspace (pfc_removeElt_to_deleteElts lastState x) ws
+    WSEManipulate (undo, x) -> if undo
+      then undefined --doCmdOwlPFWorkspaceUndoPermanentFirst (const (PFCManipulate ==> x)) ws
+      else undefined --doCmdWorkspace (PFCManipulate ==> x) ws
+    -- TODO add children to selection before moving
+    WSEMoveElt x -> undefined --doCmdWorkspace (PFCMove ==> x) ws
+    WSEResizeCanvas x -> undefined --doCmdWorkspace (PFCResizeCanvas ==> x) ws
+    WSEUndo -> undefined --undoWorkspace ws
+    WSERedo -> undefined --redoWorkspace ws
+    WSELoad x -> undefined --loadOwlPFStateIntoWorkspace (sPotatoFlow_to_pFState x) ws
+  afterState = _owlPFWorkspace_pFState r
+  isValidAfter = pFState_isValid afterState
+  in
+    if isValidAfter then r else
+      error ("INVALID " <> show evt <> "\n" <> debugPrintBeforeAfterState lastState afterState)
