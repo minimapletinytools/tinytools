36361a5362b4db372d67ae6b3e2288c3b4fd9d42
pdlla <chippermonky@gmail.com>
2021-12-25 15:16:12 -0800

    walkToRender ugh

--- src/Potato/Flow/Methods/Lines.hs
+++ src/Potato/Flow/Methods/Lines.hs
@@ -16,6 +16,9 @@ import           Data.Dependent.Sum (DSum ((:=>)), (==>))
 import           Data.Maybe         (fromJust)
 import qualified Data.Text          as T
 import qualified Potato.Data.Text.Zipper   as TZ
+import Data.Default
+
+import Linear.Vector ((^*))
 
 -- cases
 -- '⇇' '⇇'
@@ -27,7 +30,48 @@ import qualified Potato.Data.Text.Zipper   as TZ
 
 
 data CartDir = CD_Up | CD_Down | CD_Left | CD_Right
-data AnchorType = AT_End_Up | AT_End_Down | AT_End_Left | AT_End_Right | AT_Elbow_UR | AT_Elbow_RD | AT_Elbow_DL | AT_Elbow_LU
+data AnchorType = AT_End_Up | AT_End_Down | AT_End_Left | AT_End_Right | AT_Elbow_UR | AT_Elbow_RD | AT_Elbow_DL | AT_Elbow_LU | AT_Elbow_Invalid
+
+maybeIndex :: Text -> Int -> Maybe MPChar
+maybeIndex t i = if i < T.length t
+  then Just $ (Just $ T.index t i)
+  else Nothing
+
+renderLine :: SuperStyle -> CartDir -> MPChar
+renderLine SuperStyle {..} cd = case cd of
+  CD_Up -> _superStyle_vertical
+  CD_Down -> _superStyle_vertical
+  CD_Left -> _superStyle_horizontal
+  CD_Right -> _superStyle_horizontal
+
+renderLineEnd :: SuperStyle -> LineStyle -> CartDir -> Int -> MPChar
+renderLineEnd SuperStyle {..} LineStyle {..} cd distancefromend = r where
+  r = case cd of
+    CD_Up -> fromMaybe _superStyle_vertical $ maybeIndex _lineStyle_upArrows distancefromend
+    CD_Down -> fromMaybe _superStyle_vertical $ maybeIndex (T.reverse _lineStyle_downArrows) distancefromend
+    CD_Left -> fromMaybe _superStyle_horizontal $ maybeIndex _lineStyle_leftArrows distancefromend
+    CD_Right -> fromMaybe _superStyle_horizontal $ maybeIndex (T.reverse _lineStyle_rightArrows) distancefromend
+
+
+renderAnchorType :: SuperStyle -> LineStyle -> AnchorType -> MPChar
+renderAnchorType ss@SuperStyle {..} ls@LineStyle {..} at = r where
+  r = case at of
+    AT_End_Up -> renderLineEnd ss ls CD_Up 0
+    AT_End_Down -> renderLineEnd ss ls CD_Down 0
+    AT_End_Left -> renderLineEnd ss ls CD_Left 0
+    AT_End_Right -> renderLineEnd ss ls CD_Right 0
+    AT_Elbow_UR -> _superStyle_tl
+    AT_Elbow_RD -> _superStyle_tr
+    AT_Elbow_DL -> _superStyle_br
+    AT_Elbow_LU -> _superStyle_bl
+    AT_Elbow_Invalid -> Just '?'
+
+flipCartDir :: CartDir -> CartDir
+flipCartDir = \case
+  CD_Up -> CD_Down
+  CD_Down -> CD_Up
+  CD_Left -> CD_Right
+  CD_Right -> CD_Left
 
 cartDirToUnit :: CartDir -> XY
 cartDirToUnit = \case
@@ -36,12 +80,37 @@ cartDirToUnit = \case
   CD_Left -> V2 (-1) 0
   CD_Right -> V2 1 0
 
+cartDirToAnchor :: CartDir -> Maybe CartDir -> AnchorType
+cartDirToAnchor start mnext = case mnext of
+  Nothing -> case start of
+    CD_Up -> AT_End_Up
+    CD_Down -> AT_End_Down
+    CD_Left -> AT_End_Left
+    CD_Right -> AT_End_Right
+  Just next -> case start of
+    CD_Up -> case next of
+      CD_Left -> AT_Elbow_RD
+      CD_Right -> AT_Elbow_UR
+      _ -> AT_Elbow_Invalid
+    CD_Down -> case next of
+      CD_Left -> AT_Elbow_DL
+      CD_Right -> AT_Elbow_LU
+      _ -> AT_Elbow_Invalid
+    CD_Left -> case next of
+      CD_Up -> AT_Elbow_LU
+      CD_Down -> AT_Elbow_UR
+      _ -> AT_Elbow_Invalid
+    CD_Right -> case next of
+      CD_Up -> AT_Elbow_DL
+      CD_Down -> AT_Elbow_RD
+      _ -> AT_Elbow_Invalid
+
+
 data LineAnchorsForRender = LineAnchorsForRender {
   _lineAnchorsForRender_start :: XY
   , _lineAnchorsForRender_rest :: [(CartDir, Int)]
 }
 
---foldLineAnchors :: (a -> )
 
 -- TODO
 lineAnchorsForRenderToPointList :: LineAnchorsForRender -> [XY]
@@ -60,6 +129,29 @@ sSimpleLineSolver SimpleLineSolverParameters {..} (lbx1, al1) (lbx2, al2) = r wh
   LBox (V2 x1 y1) (V2 w1 h1) = lbx1
   LBox (V2 x2 y2) (V2 w2 h2) = lbx2
   r = undefined
+
+
+walkToRender :: Bool -> XY -> (CartDir, Int) -> Maybe (CartDir, Int) -> Int -> (XY, MPChar)
+walkToRender isstart begin (tcd, tl) mnext d = r where
+  currentpos = begin + (cartDirToUnit tcd) ^* d
+  ss@SuperStyle {..} = def
+  ls@LineStyle {..} = def
+  endorelbow = renderAnchorType ss ls $ cartDirToAnchor tcd (fmap fst mnext)
+  startorregular = if isstart
+    then if d < tl `div` 2
+      -- if we are at the start and near the beginning then render start of line
+      then renderLineEnd ss ls (flipCartDir tcd) d
+      else if isNothing mnext
+        -- if we are not at the start and at the end then render end of line
+        then renderLineEnd ss ls tcd (tl-d)
+        -- otherwise render line as usual
+        else renderLine ss tcd
+    else renderLine ss tcd
+  r = if d == tl
+    then (currentpos, endorelbow)
+    else (currentpos, startorregular)
+
+
 
 
 
