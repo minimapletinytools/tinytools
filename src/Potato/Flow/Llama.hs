fcb43d78fa15af80fe71a21fc59ae62122d695f0
pdlla <chippermonky@gmail.com>
2022-01-23 08:47:46 -0800

    Llama basic stuff compiles

--- src/Potato/Flow/Llama.hs
+++ src/Potato/Flow/Llama.hs
@@ -1,23 +1,72 @@
 -- WIP
 
 
-module Potato.Flow.Llama
+module Potato.Flow.Llama where
 
 import           Relude
 
+import           Potato.Flow.Owl
+import           Potato.Flow.SElts
+import Potato.Flow.OwlState
 import           Potato.Flow.Types
 
-data SLlama = SLlama_Set [(REltId, SEltLabel)]
+import qualified Data.IntMap as IM
+
+data SLlama = SLlama_Set [(REltId, SElt)] | SLlama_Rename (REltId, Text)
+
+data ApplyLlamaError = ApplyLlamaError_Generic Text
 
 data Llama = Llama {
-  _llama_apply :: OwlPFState -> (OwlPFState, SuperOwlChanges, Llama)
+  _llama_apply :: OwlPFState -> Either ApplyLlamaError (OwlPFState, SuperOwlChanges, Llama)
   , _llama_serialize :: SLlama
 }
 
-makeSetLlama :: (REltId, SEltLabel) -> Llama
-makeSetLlama (rid, seltl) = r where
-  apply = undefined
-  serialize = SLama_Set [(rid, seltl)]
+makeRenameLlama :: (REltId, Text) -> Llama
+makeRenameLlama (rid, newname) = r where
+
+  apply pfs = let
+      mapping = _owlTree_mapping . _owlPFState_owlTree $ pfs
+    in case IM.lookup rid mapping of
+        Nothing -> Left $ ApplyLlamaError_Generic $ "Element to rename does not exist " <> show rid
+        Just (oldoem, oldoe) -> let
+            (newoe, oldname) = case oldoe of
+              OwlEltFolder oi kiddos -> (OwlEltFolder (oi { _owlInfo_name = newname}) kiddos, _owlInfo_name oi)
+              OwlEltSElt oi selt -> (OwlEltSElt (oi { _owlInfo_name = newname}) selt, _owlInfo_name oi)
+            newsowl = SuperOwl rid oldoem newoe
+            newMapping = IM.insert rid (oldoem, newoe) mapping
+            changes = IM.singleton rid (Just newsowl)
+            unset = makeRenameLlama (rid, oldname)
+            newState = pfs { _owlPFState_owlTree = (_owlPFState_owlTree pfs) { _owlTree_mapping = newMapping } }
+          in
+            Right $ (newState, changes, unset)
+
+
+  serialize = SLlama_Rename (rid, newname)
+  r = Llama {
+      _llama_apply = apply
+      , _llama_serialize = serialize
+    }
+
+
+makeSetLlama :: (REltId, SElt) -> Llama
+makeSetLlama (rid, selt) = r where
+  apply pfs = let
+      mapping = _owlTree_mapping . _owlPFState_owlTree $ pfs
+    in case IM.lookup rid mapping of
+        Nothing -> Left $ ApplyLlamaError_Generic $ "Element to modify does not exist " <> show rid
+        Just (_, OwlEltFolder _ _) -> Left $ ApplyLlamaError_Generic $ "Element to modify is a folder " <> show rid
+        Just (oldoem, OwlEltSElt oi oldselt) -> let
+            newoe = OwlEltSElt oi selt
+            newsowl = SuperOwl rid oldoem newoe
+            newMapping = IM.insert rid (oldoem, newoe) mapping
+            changes = IM.singleton rid (Just newsowl)
+            unset = makeSetLlama (rid, oldselt)
+            newState = pfs { _owlPFState_owlTree = (_owlPFState_owlTree pfs) { _owlTree_mapping = newMapping } }
+          in
+            Right $ (newState, changes, unset)
+
+
+  serialize = SLlama_Set [(rid, selt)]
   r = Llama {
       _llama_apply = apply
       , _llama_serialize = serialize
@@ -26,4 +75,4 @@ makeSetLlama (rid, seltl) = r where
 
 
 sLlama_deserialize :: SLlama -> Llama
-sLlama_deserialize (SLama_Set xs) = undefined
+sLlama_deserialize (SLlama_Set xs) = undefined
---
+++ test/Potato/Flow/LlamaSpec.hs
@@ -0,0 +1,16 @@
+module Potato.Flow.LlamaSpec (
+  spec
+) where
+
+
+import           Relude
+
+import           Test.Hspec
+
+import Potato.Flow.Llama
+
+spec :: Spec
+spec = do
+  describe "Llama" $ do
+    it "TODO" $ do
+      True `shouldBe` True
