4e3205fc0d2a9230b2702d0416f26e7ba7101cd4
pdlla <chippermonky@gmail.com>
2022-06-04 17:40:37 -0700

    Add MonadPotatoLogger not sure if I"ll use it yet though...

--- src/Potato/Flow/DebugHelpers.hs
+++ src/Potato/Flow/DebugHelpers.hs
@@ -1,6 +1,7 @@
 module Potato.Flow.DebugHelpers where
 
 import           Relude
+import Control.Monad.Writer
 
 -- prob just replace with show? Why do you ever want to not potato show?
 -- the reason is becaues potatoshow doesn't show all information, but whatever it's fine
@@ -22,3 +23,18 @@ assertPotatoShowAndDump a v b = if v
 
 traceWith :: (a -> String) -> a -> a
 traceWith f x = trace (f x) x
+
+
+-- TODO decide if we want to use this??? Too much???
+data PotatoLoggerLevel = PLL_Debug | PLL_Info | PLL_Warn | PLL_Error
+data PotatoLoggerComponent = PLC_None | PLC_Goat
+
+class MonadPotatoLogger m where
+  potatoLog :: PotatoLoggerLevel -> PotatoLoggerComponent -> Text -> m ()
+
+data PotatoLoggerObject = PotatoLoggerObject PotatoLoggerLevel PotatoLoggerComponent Text
+
+type PotatoLogger = Writer [PotatoLoggerObject]
+
+instance MonadPotatoLogger PotatoLogger where
+  potatoLog l c t = tell $ [PotatoLoggerObject l c t]
