80430de49982bd3a2fba4bbcf01a384471689348
weipinglu <weipinglu@gmail.com>
2022-12-20 18:17:48 -0800

    m

--- test/Potato/Data/Text/Zipper2Spec.hs
+++ test/Potato/Data/Text/Zipper2Spec.hs
@@ -12,11 +12,43 @@ import qualified Data.Map         as Map
 import qualified          Data.Text as T
 import Control.Monad
 
-import           Potato.Data.Text.Zipper
+import           Potato.Data.Text.Zipper2
 
+tz_ = TextZipper ["this is an example content of", "a text zipper"]
+                "the capital "
+                ["TEXT IS THE SELECTED", "PORTION"]
+                " of the"
+                ["text zipper"]
+
+tz = TextZipper ["a text zipper", "this is an example content of"]
+                "the capital "
+                ["TEXT IS THE SELECTED", "PORTION"]
+                " of the"
+                ["text zipper"]
 
 spec :: Spec
 spec =
-  describe "Zipper" $ do
-    it "add test cases please" $ do
-      "TODO" `shouldBe` "TODO"
+  describe "Zipper2" $ do
+  it "leftN 1" $ do
+    let tz_1 = TextZipper   ["a text zipper", "this is an example content of"]
+                        "the capital"
+                        []
+                        " TEXT IS THE SELECTED"
+                        ["PORTION of the","text zipper"]
+    leftN 1 tz `shouldBe` tz_1
+
+  it "leftN 5" $ do
+    let tz_5 = TextZipper   ["a text zipper", "this is an example content of"]
+                            "the cap"
+                            []
+                            "ital TEXT IS THE SELECTED"
+                            ["PORTION of the","text zipper"]
+    leftN 5 tz `shouldBe` tz_5
+
+  it "leftN 20" $ do
+    let tz_20 = TextZipper  ["this is an example content of"]
+                            "a text"
+                            []
+                            " zipper"
+                            ["the capital TEXT IS THE SELECTED","PORTION of the","text zipper"]
+    leftN 20 tz `shouldBe` tz_20
