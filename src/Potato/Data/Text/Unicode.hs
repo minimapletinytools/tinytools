44859325f1f6dcd0497f0f008bda3b189b654129
pdlla <chippermonky@gmail.com>
2023-01-02 12:26:13 -0800

    more grapheme cluster functions

--- src/Potato/Data/Text/Unicode.hs
+++ src/Potato/Data/Text/Unicode.hs
@@ -20,14 +20,45 @@ getCharWidth = fromIntegral . TZ.charWidth
 removeWideChars :: Text -> Text
 removeWideChars = T.filter (\c -> getCharWidth c <= 1) 
 
+internal_getCharacterBreaks :: Text -> [ICU.Break ()]
+internal_getCharacterBreaks input = r where 
+  breaker = ICU.breakCharacter ICU.Current 
+  r = ICU.breaks breaker input
+
+-- | True if the Text is a single grapheme cluster, False otherwise
+isSingleGraphemeCluster :: Text -> Bool
+isSingleGraphemeCluster input = r where
+  tbreaks = internal_getCharacterBreaks input
+  r = case tbreaks of 
+    -- no characters, not a grapheme cluster
+    [] -> False 
+    -- only one break, it's a grapheme cluster if it has more than one unicode char in it
+    (b:[]) -> T.length (ICU.brkBreak b) > 1
+    -- more than one character break
+    _ -> False 
+
+-- | True if the last character in the text is a single grapheme cluster, False otherwise
+endsInGraphemeCluster :: Text -> Bool
+endsInGraphemeCluster input = r where
+  tbreaks' = internal_getCharacterBreaks input
+  gotoend tbreaks = case tbreaks of 
+    [] -> False
+    (b:[]) -> isSingleGraphemeCluster (ICU.brkBreak b)
+    (_:bs) -> gotoend bs
+  r = gotoend tbreaks'
+
 -- most terminals do not support grapheme clusters right now :( 👎🏻👎🏼👎🏽👎🏾👎🏿
+-- | removes grapheme clusters from the text and replaces them with the first character in the cluster
 removeGraphemeCluster :: Text -> Text
 removeGraphemeCluster input = r where
-  breaker = ICU.breakCharacter ICU.Current 
-  tbreaks = ICU.breaks breaker input
+  tbreaks = internal_getCharacterBreaks input
   -- if there is more than one character in the break then it must have been a grapheme cluster
   -- so just use the first character
   fmapfn b = case T.uncons (ICU.brkBreak b) of
     Nothing -> ""
     Just (c, _) -> T.singleton c
   r = mconcat $ fmap fmapfn tbreaks
+
+-- | True if the input text contains a grapheme cluster
+containsGraphemeCluster :: Text -> Bool
+containsGraphemeCluster input = removeGraphemeCluster input /= input
--- test/Potato/Data/Text/UnicodeSpec.hs
+++ test/Potato/Data/Text/UnicodeSpec.hs
@@ -16,6 +16,23 @@ import           Potato.Data.Text.Unicode
 
 spec :: Spec
 spec = describe "Unicode" $ do
+  it "isSingleGraphemeCluster" $ do
+    isSingleGraphemeCluster "👎🏼" `shouldBe` True
+    isSingleGraphemeCluster "👎" `shouldBe` False
+    isSingleGraphemeCluster "" `shouldBe` False
+    isSingleGraphemeCluster "a" `shouldBe` False
+    isSingleGraphemeCluster "👎🏻👎🏼👎🏽👎🏾👎🏿" `shouldBe` False
+  it "endsInGraphemeCluster" $ do
+    endsInGraphemeCluster "👎🏼" `shouldBe` True
+    endsInGraphemeCluster "👎🏿" `shouldBe` True
+    endsInGraphemeCluster "" `shouldBe` False
+    endsInGraphemeCluster "a" `shouldBe` False
+    endsInGraphemeCluster "👎🏻👎🏼👎🏽👎🏾👎🏿" `shouldBe` True
+    endsInGraphemeCluster "👎🏻👎🏼👎🏽👎🏾👎🏿😱" `shouldBe` False
+    endsInGraphemeCluster "goop😱dedoop👎🏿" `shouldBe` True
   it "removeGraphemeCluster" $ do
     removeGraphemeCluster "👎🏻👎🏼👎🏽👎🏾👎🏿" `shouldBe` "👎👎👎👎👎"
+  it "containsGraphemeCluster" $ do
+    containsGraphemeCluster "👎👎👎👎👎" `shouldBe` False
+    containsGraphemeCluster "👎👎👎🏿👎👎👎" `shouldBe` True
     
