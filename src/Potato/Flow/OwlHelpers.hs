f65625a1279c004bd0d202ff204aae48223940cc
pdlla <chippermonky@gmail.com>
2023-02-17 20:45:13 -0800

    flip line

--- src/Potato/Flow/OwlHelpers.hs
+++ src/Potato/Flow/OwlHelpers.hs
@@ -7,6 +7,7 @@ import Potato.Flow.SElts
 import Potato.Flow.Owl
 import Potato.Flow.OwlItem
 import Potato.Flow.Llama
+import Potato.Flow.SEltMethods
 
 superOwl_mustGetSLine :: SuperOwl -> SAutoLine
 superOwl_mustGetSLine sowl = case _owlItem_subItem$ _superOwl_elt sowl of
@@ -31,6 +32,21 @@ makeLlamaForLineStyle sowl end newstyle = r where
   sline = superOwl_mustGetSLine sowl
   newsline = sline {
       _sAutoLine_lineStyle = if setLineStyleEnd_setStart end then newstyle else _sAutoLine_lineStyle sline
-      , _sAutoLine_lineStyleEnd = if setLineStyleEnd_setEnd end then newstyle else _sAutoLine_lineStyleEnd sline 
+      , _sAutoLine_lineStyleEnd = if setLineStyleEnd_setEnd end then newstyle else _sAutoLine_lineStyleEnd sline
     }
   r = makeSetLlama (rid, SEltLine newsline)
+
+makeLlamaForFlipLineStyle :: SuperOwl -> Maybe Llama
+makeLlamaForFlipLineStyle sowl = r where
+  seltl = superOwl_toSEltLabel_hack sowl
+  startStyle = getSEltLabelLineStyle seltl
+  endStyle = getSEltLabelLineStyleEnd seltl
+  rid = _superOwl_id sowl
+  sline = superOwl_mustGetSLine sowl
+  newsline = sline {
+      _sAutoLine_lineStyle = _sAutoLine_lineStyleEnd sline
+      , _sAutoLine_lineStyleEnd = _sAutoLine_lineStyle sline
+    }
+  r = if startStyle == endStyle
+    then Nothing
+    else Just $ makeSetLlama (rid, SEltLine newsline)
