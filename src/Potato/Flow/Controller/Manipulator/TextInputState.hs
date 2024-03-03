ae41cad6c149c525759781c4dac2d4581f624430
pdlla <chippermonky@gmail.com>
2024-03-03 10:08:00 -0800

    pull out functionality into TextInputState

--- src/Potato/Flow/Controller/Manipulator/TextInputState.hs
+++ src/Potato/Flow/Controller/Manipulator/TextInputState.hs
@@ -8,11 +8,18 @@ import           Potato.Flow.Math
 import           Potato.Flow.Serialization.Snake
 import           Potato.Flow.Controller.Input
 import           Potato.Flow.Controller.Handler
+import Potato.Flow.Owl
+import Potato.Flow.Llama
+import           Potato.Flow.Serialization.Snake
+import          Potato.Flow.Types
 
+
+import qualified Data.IntMap as IM
 import qualified Data.Text as T
 import qualified Potato.Data.Text.Zipper                          as TZ
 import qualified Data.Map as Map
 
+import Control.Exception (assert)
 
 data TextInputState = TextInputState {
   _textInputState_rid            :: REltId
@@ -65,6 +72,34 @@ inputSingleLineZipper tais kk = (changed, tais { _textInputState_zipper = newZip
     _ -> (False, oldZip)
 
 
+-- TODO support shift selecting text someday meh
+-- | returns zipper in TextInputState after keyboard input has been applied
+-- Bool indicates if there was any real input
+inputBoxTextZipper :: TextInputState -> KeyboardKey -> (Bool, TextInputState)
+inputBoxTextZipper tais kk = (changed, tais { _textInputState_zipper = newZip }) where
+
+  oldZip = _textInputState_zipper tais
+  (changed, newZip) = case kk of
+    KeyboardKey_Left    -> (False, TZ.left oldZip)
+    KeyboardKey_Right   -> (False, TZ.right oldZip)
+    KeyboardKey_Up      -> (False, TZ.up oldZip)
+    KeyboardKey_Down    -> (False, TZ.down oldZip)
+    KeyboardKey_Home    -> (False, TZ.home oldZip)
+    KeyboardKey_End -> (False, TZ.end oldZip)
+    KeyboardKey_PageUp -> (False, TZ.pageUp 5 oldZip)
+    KeyboardKey_PageDown -> (False, TZ.pageDown 5 oldZip)
+
+    KeyboardKey_Return  -> (True, TZ.insertChar '\n' oldZip)
+    KeyboardKey_Space   -> (True, TZ.insertChar ' ' oldZip)
+    KeyboardKey_Delete  -> (True, TZ.deleteRight oldZip)
+    KeyboardKey_Backspace -> (True, TZ.deleteLeft oldZip)
+    KeyboardKey_Char c  -> (True, TZ.insertChar c oldZip)
+    KeyboardKey_Paste t -> (True, TZ.insert t oldZip)
+
+    k                   -> error $ "unexpected keyboard char (event should have been handled outside of this handler)" <> show k
+
+
+
 makeTextHandlerRenderOutput :: TextInputState -> HandlerRenderOutput
 makeTextHandlerRenderOutput btis = r where
   dls = _textInputState_displayLines btis
@@ -91,3 +126,85 @@ makeTextHandlerRenderOutput btis = r where
     return [cursorh]
 
   r = HandlerRenderOutput $ fromMaybe [] mlbox
+
+
+
+
+data TextImpl o = TextImpl {
+  _textImpl_mustGetOwlItem :: CanvasSelection -> (REltId, o)
+  --, _textImpl_updateTextInputStateWithOwlItem :: o -> TextInputState -> TextInputState
+  , _textImpl_owlItemText :: o -> Text
+  , _textImpl_owlItemBox :: o -> CanonicalLBox
+  , _textImpl_owlItemAlignment :: o -> TextAlign
+
+  , _textImpl_inputOwlItemZipper :: TextInputState -> KeyboardKey -> (Bool, TextInputState)
+
+
+  -- TODO replace this with something higher level so that you can maybe use this for AutoLineLabel as well
+  , _textImpl_makeController :: Text -> Text -> Controller
+  -- TODO need something to make mouse up action generic
+}
+
+
+updateTextInputStateWithOwlItem :: TextImpl o -> o -> TextInputState -> TextInputState
+updateTextInputStateWithOwlItem impl oitem btis = r where
+  alignment = convertTextAlignToTextZipperTextAlignment . (_textImpl_owlItemAlignment impl) $ oitem
+  CanonicalLBox _ _ newBox@(LBox _ (V2 width _)) = _textImpl_owlItemBox impl $ oitem
+  r = btis {
+      _textInputState_box = newBox
+      , _textInputState_displayLines = TZ.displayLinesWithAlignment alignment width () () (_textInputState_zipper btis)
+    }
+
+-- TODO I think you need to pad empty lines in the zipper to fill out the box D:
+-- ok, no you don't, that's only for the non-paragraph text area that we don't actually have yet
+makeOwlItemTextInputState :: TextImpl o -> REltId -> o -> RelMouseDrag -> TextInputState
+makeOwlItemTextInputState impl rid oitem rmd = r where
+  ogtext = _textImpl_owlItemText impl $ oitem
+  ogtz = TZ.fromText ogtext
+  r' = TextInputState {
+      _textInputState_rid = rid
+      , _textInputState_original   = Just ogtext
+      , _textInputState_zipper   = ogtz
+
+      -- these fields get updated in next pass
+      , _textInputState_box = error "expected to be filled"
+      , _textInputState_displayLines = error "expected to be filled"
+
+      --, _textInputState_selected = 0
+    }
+  r'' = updateTextInputStateWithOwlItem impl oitem r'
+  r = mouseText r'' rmd
+
+
+inputOwlItem :: TextImpl o -> TextInputState -> SuperOwl -> KeyboardKey -> (TextInputState, Maybe Llama)
+inputOwlItem impl tais sowl kk = (newtais, mop) where
+  (changed, newtais) = _textImpl_inputOwlItemZipper impl tais kk
+  origt = _textInputState_original tais
+  newt = TZ.value (_textInputState_zipper newtais)
+  controller = _textImpl_makeController impl (fromMaybe "" origt) newt
+  mop = if changed
+    then Just $ makePFCLlama . OwlPFCManipulate $ IM.fromList [(_superOwl_id sowl,controller)]
+    else Nothing
+
+
+-- TODO rename to updateOwlItemTextInputStateWithSelection
+updateOwlItemTextInputState :: TextImpl o -> Bool -> CanvasSelection -> TextInputState -> TextInputState
+updateOwlItemTextInputState impl reset selection tais = assert tzIsCorrect r where
+  (_, oitem) = _textImpl_mustGetOwlItem impl $ selection
+
+  newText = _textImpl_owlItemText impl oitem
+
+  recomputetz = TZ.fromText newText
+  oldtz = _textInputState_zipper tais
+  -- NOTE that recomputetz won't have the same cursor position
+  -- TODO delete this check, not very meaningful, but good for development purposes I guess
+  tzIsCorrect = TZ.value oldtz == TZ.value recomputetz
+
+  nextstate = updateTextInputStateWithOwlItem impl oitem tais
+
+  r = if reset
+    then nextstate {
+        _textInputState_original = Just newText
+      }
+    else nextstate
+
